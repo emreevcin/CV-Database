aba8b60516348aa551620cdce586aea81973966c
tugaysarsilmaz <sevkisarsilmaz@gmail.com>
2022-12-20 23:55:28 +0300

    new: Import function is added

--- src/com/Controllers/MainController.java
+++ src/com/Controllers/MainController.java
@@ -7,10 +7,15 @@ import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
+import javafx.scene.control.ListView;
 import javafx.stage.Stage;
-
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.stage.FileChooser;
 import java.awt.*;
+import java.io.*;
 import java.net.URL;
 import java.util.ResourceBundle;
 
@@ -26,7 +31,10 @@ public class MainController implements Initializable {
 
     @FXML
     private Button createCV ;
-
+    @FXML
+    private ListView cvList;
+    @FXML
+    private ListView pdfList;
     private Scene addScene ;
     private Stage addStage;
     @FXML
@@ -90,7 +98,7 @@ public class MainController implements Initializable {
         controller_s5_ro.init(this);
         cvList.getItems().add("CV1"); // CV İÇİN STATİK BİR ÖRNEK EKLEDİM.
         cvList.getItems().add("CV2"); // CV İÇİN GİRİLEN TİTLE PDF NAME OLMALI
-
+        pullFiles();
     }
 
     @FXML
@@ -129,3 +137,87 @@ public class MainController implements Initializable {
 
 
     }
+
+    @FXML
+    private void openButtonClick(ActionEvent event) throws IOException {
+
+        File fileToOpen = new File("./././pdfs/"+pdfList.getSelectionModel().getSelectedItem());
+
+        if(!fileToOpen.exists()){
+            String alertText = "File does not exist!";
+            Alerting(alertText);
+            pullFiles();
+        }else {
+            Desktop.getDesktop().open(fileToOpen);
+        }
+    }
+    private void Alerting(String contentText){
+        Alert alert = new Alert(Alert.AlertType.INFORMATION);
+        alert.setTitle("Error");
+        alert.setHeaderText("PROBLEM:");
+        alert.setContentText(contentText);
+        alert.show();
+    }
+    private   void pullFiles(){
+        File folder = new File("./././pdfs/");
+        File[] listOfFiles = folder.listFiles();
+        pdfList.getItems().clear();
+        for(File file : listOfFiles){
+            if(file.isFile()){
+                pdfList.getItems().add(file.getName());
+            }
+        }
+    }
+    private static void copyFileUsingStream(File source, File dest) throws IOException {
+        InputStream is = null;
+        OutputStream os = null;
+        try {
+            is = new FileInputStream(source);
+            os = new FileOutputStream(dest);
+            byte[] buffer = new byte[1024];
+            int length;
+            while ((length = is.read(buffer)) > 0) {
+                os.write(buffer, 0, length);
+            }
+        } finally {
+            is.close();
+            os.close();
+        }
+    }
+
+    @FXML
+    private void addButtonClick(ActionEvent event) throws IOException {
+        FileChooser fc = new FileChooser();
+        fc.getExtensionFilters().addAll(new FileChooser.ExtensionFilter("PDF (*.PDF, *.pdf)", "*.pdf","*.PDF"));
+        File f = fc.showOpenDialog(null);
+
+        if(f!=null){
+            File src = new File(f.getPath());
+            File dest = new File("./././pdfs/"+f.getName());
+            if (dest.exists()){
+                String alertText = "File already exist!";
+                Alerting(alertText);
+            }else{
+                copyFileUsingStream(src,dest);
+                pullFiles();
+            }
+        }
+    }
+
+    @FXML
+    private void deleteButtonClick(ActionEvent event) throws  IOException{
+        File fileToDelete = new File("./././pdfs/"+pdfList.getSelectionModel().getSelectedItem());
+        if(!fileToDelete.exists()){
+            String alertText = "File does not exist!";
+            Alerting(alertText);
+        }
+        else{
+            fileToDelete.delete();
+        }
+        pullFiles();
+    }
+
+
+
+
+}
